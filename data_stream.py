561e6bde7d2f019c5cb6ba124fdb75ce43f14b8c
Ziyad Alkhonein <ziyadalkhonein@gmail.com>
2021-02-24 22:07:03 +0300

    Update data_stream.py

--- data_stream.py
+++ data_stream.py
@@ -5,84 +5,65 @@ from pyspark.sql.types import *
 import pyspark.sql.functions as psf
 
 
-# TODO Create a schema for incoming resources
-schema = StructType([ 
-    StructField("crime_id", IntegerType(), True),
+
+schema = StructType([
+    StructField("crime_id", StringType(), True),
     StructField("original_crime_type_name", StringType(), True),
-    StructField("report_date", TimestampType(), True),
-    StructField("call_date", TimestampType(), True),
-    StructField("offense_date", TimestampType(), True),
-    StructField("call_time", TimestampType(), True),
+    StructField("report_date", StringType(), True),
+    StructField("call_date", StringType(), True),
+    StructField("offense_date", StringType(), True),
+    StructField("call_time", StringType(), True),
     StructField("call_date_time", TimestampType(), True),
     StructField("disposition", StringType(), True),
     StructField("address", StringType(), True),
     StructField("city", StringType(), True),
     StructField("state", StringType(), True),
-    StructField("agency_id", IntegerType(), True),
+    StructField("agency_id", StringType(), True),
     StructField("address_type", StringType(), True),
     StructField("common_location", StringType(), True)
 ])
 
 def run_spark_job(spark):
 
-    # TODO Create Spark Configuration
-    # Create Spark configurations with max offset of 200 per trigger
+    # Spark configurations with max offset of 200 per trigger
     # set up correct bootstrap server and port
     df = spark \
-        .readStream \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("startingOffsets", "earliest") \
-        .option("subscribe", "test") \
-        .option("spark.sql.shuffle.partitions",10)\
-        .option("spark.executor.heartbeatInterval",20)\
-        .option("spark.default.parallelism",100)\
+        .readStream\
+        .format("kafka")\
+        .option("kafka.bootstrap.servers","localhost:9092")\
+        .option("subscribe","test")\
+        .option("startingOffsets","earliest")\
         .load()
 
-
-    # Show schema for the incoming resources for checks
+    # schema for the incoming resources for checks
     df.printSchema()
-    print('------------')
 
-    # TODO extract the correct column from the kafka input resources
+    # extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("cast(value as String) value")
+    kafka_df = df.selectExpr("CAST(value as STRING)")
     
-    kafka_df.printSchema()
-
     service_table = kafka_df\
         .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
-        .select("DF.*")
-
-    # TODO select original_crime_type_name and disposition
+        .select("DF.*")    
+    
+    # select original_crime_type_name and disposition
     distinct_table = service_table.selectExpr("original_crime_type_name","disposition")
     
-    #distinct_table.printSchema()
-    #distinct_table.awaitTermination()
-
     
-    #distinct_table.show(n=5)
-    
-    #distinct_table.createOrReplaceTempView("distinct_table")
-
-    #spark.sql('select * from distinct_table limit 10').show()
     # count the number of original crime type
     agg_df = distinct_table.groupBy('original_crime_type_name').count()
 
-    # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
-    # TODO write output stream
+    agg_df.printSchema()
+
+    # write output stream
+    query = agg_df\
+            .writeStream\
+            .outputMode("Complete")\
+            .format("console")\
+            .start()
+
     
-    query = agg_df \
-        .writeStream \
-        .trigger(processingTime="30 seconds") \
-        .outputMode('Complete') \
-        .format('console') \
-        .option("truncate", "false") \
-        .start()
-
-
-
-    # TODO attach a ProgressReporter
+    # attach a ProgressReporter
     query.awaitTermination()
 
     # TODO get the right radio code json path
@@ -101,17 +82,19 @@ def run_spark_job(spark):
 
 
     join_query.awaitTermination()
+    
+    
 
-
+    
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    # TODO Create Spark in Standalone mode
+    # Create Spark in Standalone mode
     spark = SparkSession \
         .builder \
         .master("local[*]") \
-        .config("spark.ui.port",3000)\
         .appName("KafkaSparkStructuredStreaming") \
+        .config("spark.ui.port",3000)\
         .getOrCreate()
 
     logger.info("Spark started")
